988bacf4b2a29b796860cfa8e0ad28c97abe93b5
EvanEbert <evan.ebert17@gmail.com>
2021-06-30 20:53:49 -0500

    added html elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,28 @@
-console.log("shit");
+//todo list:
+// global variables that basically just target HTML elemtns that we are going to reference
+var headerEl
+var paragraphEl
+var highScore
+var buttonEl
+var timerText
+var h2El
+var 
+
+//array that stores questions
+//array that stores answers
+
+//-----------------
+//general things needed;
+//timer that goes down by 10 every time a wrong answer gets chosen
+//localstorage the Highscores to be displayed
+// with the highscores, a way to add a name / unique value to each of the highscores
+//highscores determined by time left on timer, sort low to high
+//"view highscores" button
+//when "startquiz" is pressed, run function that is the whole quiz
+//hiding and generating the whole quiz elements (probably display="none" or .style="none" or .stlye="")
+//function that generates html elements dynamically on new page load
+//when a button is clicked (tenative) display little message at the bottom "wrong" or "correct"
+//randomly generate the order of the questions so that you dont always have the same order or layout of the answers (tenative)
+//------------------
+
+//
